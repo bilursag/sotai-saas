27651c64797cddab80ebfac6ca071fad6a4f7495
Francisco Munoz <franciscomuniozs@gmail.com>
2025-03-10 16:51:21 -0300

    Fixed text size in hero section

--- src/components/cta-section.tsx
+++ src/components/cta-section.tsx
@@ -18,7 +18,7 @@ export function CTASection() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-4xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        className="text-4xl px-4 md:text-3xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
         La forma más rápida y fácil de generar {" "}
         <Highlight className="text-black dark:text-white">
